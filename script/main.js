b24561523f73b82066869fc9876a976739531ca4
Veaman <80166653+Veaman@users.noreply.github.com>
2025-02-14 11:20:07 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,57 @@
+const messages = [
+"Нажми на экран",  
+"С новым годом",  
+"ой",  
+"вернее",  
+"с",  
+"днём",  
+"валентина",  
+"Нажми на сердечко"
+];
+let currentPage = 0;
+let isLastPage = false;
+function showMessage() {
+    $('.message').text(messages[currentPage]);
+    
+    isLastPage = currentPage === messages.length - 1;
+    
+    if (isLastPage) {
+        $('.next-button').show();
+        $('.bg_heart').css('cursor', 'default');
+    } else {
+        $('.next-button').hide();
+        $('.bg_heart').css('cursor', 'pointer');
+    }
+}
+
+$('.bg_heart').on('click', function() {
+    if (!isLastPage) {
+        currentPage++;
+        showMessage();
+    }
+});
+
+var love = setInterval(function() {
+    var r_num = Math.floor(Math.random() * 75) + 1;
+    var r_size = Math.floor(Math.random() * 15) + 10;
+    var r_left = Math.floor(Math.random() * 100) + 1;
+    var r_bg = Math.floor(Math.random() * 25) + 100;
+    var r_time = Math.floor(Math.random() * 5) + 5;
+    
+    $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgb(255, 0, 0);animation:love " + r_time + "s ease'></div>");
+    
+    $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255,0,0);animation:love " + (r_time + 5) + "s ease'></div>");
+    
+    $('.heart').each(function() {
+        var top = parseFloat($(this).css("top"));
+        var width = parseFloat($(this).css("width"));
+        if (top <= -100 || width >= 150) {
+            $(this).remove();
+        }
+    });
+}, 500);
+showMessage();
+window.onload = function() {
+    clearMusicState(); 
+}
+document.addEventListener('DOMContentLoaded', setupMusic);
